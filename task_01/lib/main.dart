03d9ee9d94cf5929fdf3d1c221d0c6a036c28487
Yuki Takasu <t.yuki0370@gmail.com>
2022-08-17 22:46:07 +0900

    不要なコードの削除

--- task_01/lib/main.dart
+++ task_01/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
 import 'package:task_01/pages/dummy.dart';
 import 'package:task_01/util/common.dart';
 
@@ -30,7 +29,6 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() {
-    // print("create state");
     _commonFunc.showToastMsg("call create state");
     return _MyHomePageState();
   }
@@ -42,7 +40,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // print("set state. counterをインクリメント");
       _commonFunc.showToastMsg("set state. counterをインクリメント");
       _counter++;
     });
@@ -61,20 +58,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    // print("call init state");
     _commonFunc.showToastMsg("call init state");
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    // print("call did change dependencies");
     _commonFunc.showToastMsg("call did change dependencies");
   }
 
   @override
   Widget build(BuildContext context) {
-    // print("call build");
     _commonFunc.showToastMsg("call build");
 
     return Scaffold(
@@ -106,14 +100,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void deactivate() {
     super.deactivate();
-    // print("call deactivate");
     _commonFunc.showToastMsg("call deactivate");
   }
 
   @override
   void dispose() {
     super.dispose();
-    // print("call dispose");
     _commonFunc.showToastMsg("call dispose");
   }
 }
--- task_01/lib/util/common.dart
+++ task_01/lib/util/common.dart
@@ -4,10 +4,11 @@ import 'package:fluttertoast/fluttertoast.dart';
 class CommonFunc {
   void showToastMsg(msg) {
     Fluttertoast.showToast(
-        msg: msg,
-        fontSize: 16.0,
-        textColor: Colors.white,
-        backgroundColor: Colors.black54,
-        timeInSecForIosWeb: 4);
+      msg: msg,
+      fontSize: 16.0,
+      textColor: Colors.white,
+      backgroundColor: Colors.black54,
+      timeInSecForIosWeb: 1,
+    );
   }
 }
